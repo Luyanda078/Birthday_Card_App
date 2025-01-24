09b3063524075d1f9981dcd3e219ed18fe09e7cc
Luyanda <luyandashamase5@gmail.com>
2025-01-24 14:35:37 +0200

    Updated pickImage function to use ImagePicker.MediaType.IMAGE for compatibility with the latest API

--- BirtthdayCardApp/App.js
+++ BirtthdayCardApp/App.js
@@ -1,17 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
-import { StyleSheet } from 'react-native';
-
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
 import React, { useState } from 'react';
 import {
   View,
